99c6381940b5d8b49177273499215ed904c6ae62
verde <andres.laverde@protonmail.com>
2025-02-21 21:51:27 -0800

    add user tab navigation

--- effie-mobile/app/(tabs)/_layout.tsx
+++ effie-mobile/app/(tabs)/_layout.tsx
@@ -2,7 +2,7 @@ import { Tabs } from 'expo-router';
 import Ionicons from '@expo/vector-icons/Ionicons';
 
 
-export default const TabLayout = () => {
+export default function TabLayout() {
     return (
         <Tabs>
             <Tabs.Screen name="index" options={{ title: 'Chat',
--- effie-mobile/app/(tabs)/user.tsx
+++ effie-mobile/app/(tabs)/user.tsx
@@ -1,1 +1,17 @@
 //User page
+
+import { Text, View } from "react-native";
+
+export default function User() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>User profile would go here</Text>
+    </View>
+  );
+}
