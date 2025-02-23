3d02ba82c25f6da1ea88b8810a84a432eaccaa08
verde <andres.laverde@protonmail.com>
2025-02-22 18:34:25 -0800

    fixed styling

--- effie-mobile/components/SendBox.jsx
+++ effie-mobile/components/SendBox.jsx
@@ -7,9 +7,9 @@ export default function SendBox() {
   return (
     <View style={styles.sendBoxContainer}>
       <TextInput
-        style={styles.input} // Added style
-        placeholder="Type a message..." // Optional placeholder
-        placeholderTextColor="#888" // Adjusted color
+        style={styles.input} 
+        placeholder="Type a message..." 
+        placeholderTextColor="#888" 
       />
       <TouchableOpacity style={styles.sendButton}>
         {/* <Ionicons name="send" size={24} color="#006748" /> */}
@@ -20,28 +20,28 @@ export default function SendBox() {
 
 const styles = StyleSheet.create({
   sendBoxContainer: {
-    position: 'absolute', // Fix it to the bottom
-    bottom: hp(1.2), // Align at the bottom
+    position: 'absolute',
+    bottom: hp(1.2), 
     flex: 1,
     flexDirection: 'row',
     alignItems: 'center',
-    width: wp(80),
-    height: hp(8),
-    paddingHorizontal: wp(1),
+    width: wp(95),
+    height: hp(7),
+    paddingHorizontal: wp(2),
     paddingVertical: hp(1.5),
-    backgroundColor: 'white', // Ensure visibility
+    backgroundColor: 'white', 
     borderRadius: 15,
-    shadowColor: '#000', // Optional: Adds shadow for a lifted effect
+    shadowColor: '#000', 
     shadowOffset: { width: 0, height: -2 },
     shadowOpacity: 0.1,
     shadowRadius: 4,
-    elevation: 5, // Shadow for Android
+    elevation: 5, 
   },
   input: {
     height: hp(6),
     backgroundColor: '#f0f0f0',
     borderRadius: 20,
-    paddingHorizontal: wp(1),
+    paddingHorizontal: wp(2),
     fontSize: hp(2),
     color: 'black',
     flex: 1,
