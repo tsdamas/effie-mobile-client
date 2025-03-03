ee5d113cc243f05d273aeda29a3cab06126a9303
verde <andres.laverde@protonmail.com>
2025-03-02 18:51:15 -0800

    add authentication navigation logic

---
+++ effie-mobile/context/authContext.js
@@ -0,0 +1,56 @@
+import { createContext, useContext, useEffect, useState } from "react";
+
+export const AuthContext = createContext();
+
+export const AuthContextProvider = ({children}) => {
+    const [user, setUser] = useState(null);
+    const [isAuthenticated, setIsAuthenticated] = useState(undefined);
+
+    useEffect(() => {
+        //onAuthStateChanged
+
+        setTimeout(() => {
+            setIsAuthenticated(true);
+        }, 5000);
+
+        setTimeout(() => {
+            setIsAuthenticated(false);
+        }, 15000);
+    }, []);
+
+    const login = async (email, password) => {
+        try {
+
+        } catch(error) {
+
+        }
+    }
+    const logout = async () => {
+        try {
+
+        } catch(error) {
+
+        }
+    }
+    const register = async (email, password) => {
+        try {
+
+        } catch(error) {
+
+        }
+    }
+
+    return (
+        <AuthContext.Provider value={{user, isAuthenticated, login, register, logout}}>
+            {children}
+        </AuthContext.Provider>
+    );
+}
+
+export const useAuth = () => {
+    const value = useContext(AuthContext);
+    if(!value) {
+        throw new Error('useAuth must be wrapped inside AuthContextProvider');
+    }
+    return value;
+}
